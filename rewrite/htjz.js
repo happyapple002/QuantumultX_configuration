9710946120031136f836aa5a3df18b123e806fca
WilliamSteven <50542287+StevenWilliam-star@users.noreply.github.com>
2023-01-13 13:52:22 +0800

    Create htjz.js

--- rewrite/htjz.js
+++ rewrite/htjz.js
@@ -1,3 +1,14 @@
-var body = JSON.parse($response.body);
-console.log(body)
-$done();
+var obj = JSON.parse($response.body);
+ obj={
+ "data": {
+  "level": 2,
+  "status": 1,
+  "openTime": "2099-10-20",
+  "startTime": "2099-10-20",
+  "endTime": "2099-10-20",
+  "shareToken": ""
+ },
+ "code": 0,
+ "msg": "返回成功"
+};
+$done({body: JSON.stringify(obj)});
