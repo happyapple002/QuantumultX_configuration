811c718381290733b116c951ebc974ee51f2e7b7
WilliamSteven <50542287+StevenWilliam-star@users.noreply.github.com>
2023-02-08 13:39:03 +0800

    Update jianying.js

--- rewrite/jianying.js
+++ rewrite/jianying.js
@@ -19,36 +19,3 @@ if ($request.url.indexOf('user_wallet_info') != -1){
   }
 }
 $done(JSON.stringify(obj));
-
-
-/**
-var body = JSON.parse($response);
-console.log('response---------',body)
-var header = JSON.parse($header);
-if ($request.url.indexOf('user_wallet_info') != -1){
-  body = {
-    "sign" : "MJEUGpMz8xv2zeY4O5Jk7MrjVVYFfqlby8Fa+N9jVoT5gJXWqHqlP974pdmfENakn7S0+X25Wzjj4GYsXt/Z3jZdhjAxaZtZBID8wvQ29czvrjREHfLIEbLJmz2N57WY8dZQ4HsVxZtTNEBYP7R3LMU/u7nMDbdL4z9KOiWKlhU=",
-    "data" : {
-      "total_money" : 0,
-      "latest_record_time" : 0,
-      "user_id" : 2392174938823367,
-      "current_money" : 0,
-      "currency_code" : "CNY",
-      "can_buy_template_free" : true
-    },
-  "response" : {
-    "user_id":2392174938823367,
-    "total_money":0,
-    "current_money":0,
-    "currency_code":"CNY",
-    "latest_record_time":0,
-    "can_buy_template_free":true
-  },
-  "systime" : "1675816450429",
-  "errmsg" : "success",
-  "ret" : "0"
-  }
-}
-$done({body : JSON.stringify(body)});
-
-**/
